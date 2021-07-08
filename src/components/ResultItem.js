0a17282448d791e3b8aeecb9641277a11607daab
Kalyan-Sunkara <kalyant.sunkara@outlook.com>
2021-07-07 19:50:12 -0700

    Added phone functionality

--- src/components/ResultItem.js
+++ src/components/ResultItem.js
@@ -2,10 +2,22 @@ import './ResultItem.css'
 
 const ResultItem = (props) => {
   return(
-    <div className="results_div">
-    <div className="results_spacing">
-      <p className="result__style_1">{props.equation}</p>
-      <p className="result__style_2">{props.result}</p>
+    <div>
+    <div className="Desktop">
+      <div className="results_div">
+      <div className="results_spacing">
+        <p className="result__style_1">{props.equation}</p>
+        <p className="result__style_2">{props.result}</p>
+      </div>
+      </div>
+    </div>
+    <div className="Phone">
+      <div className="results_div_phone">
+      <div className="results_spacing">
+        <p className="result__style_1_phone">{props.equation}</p>
+        <p className="result__style_2_phone">{props.result}</p>
+      </div>
+      </div>
     </div>
     </div>
   )
--- src/components/ResultList.js
+++ src/components/ResultList.js
@@ -5,6 +5,8 @@ const ResultList = (props) =>{
     props.onClear()
   }
   return(
+    <div>
+    <div className="Desktop">
     <div className="ResultList__card">
     <div className="top">
       <h2 className="ResultList__header">Previous Results</h2>
@@ -20,6 +22,26 @@ const ResultList = (props) =>{
       ))}
       </ul>
       </div>
+      </div>
+      <div className="phone">
+        <div className="ResultList__card__phone">
+        <div className="top">
+          <h2 className="ResultList__header__phone">Previous Results</h2>
+          <button onClick={clearHandler} className="Clear__Button__Style__phone">Clear</button>
+          </div>
+          <ul className="ResultList__ul">
+          {props.items.map((item) => (
+          <ResultItem
+          key={item.id}
+          result={item.result}
+          equation={item.equation}
+          />
+          ))}
+          </ul>
+          </div>
+      </div>
+      </div>
+
   )
 }
 export default ResultList
